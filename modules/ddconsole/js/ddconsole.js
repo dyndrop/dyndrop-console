62a79af4ef911e8dc7306a9f2cba2fe12c8f4594
Nicolas Deschildre <ndeschildre@gmail.com>
2013-05-25 17:30:27 +0200

    We need more scope from github!

--- modules/ddconsole/js/ddconsole.js
+++ modules/ddconsole/js/ddconsole.js
@@ -22,7 +22,7 @@ angular.module('ddconsole', ['ddconsole.app', 'ddconsole.settings', 'ddconsole.r
     //If unauthorized, redirect to auth
     if($.cookie('dyndrop-token') == undefined) {
       if($location.path() != '/oauth/github/callback') {
-        window.location = "https://github.com/login/oauth/authorize?client_id=" + DDConsoleConfig.github_client_id
+        window.location = "https://github.com/login/oauth/authorize?scope=repo&client_id=" + DDConsoleConfig.github_client_id
       }
     }
     
